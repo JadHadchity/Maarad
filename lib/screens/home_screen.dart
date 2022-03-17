37c769bca1feb3af7dde22b2a46b554877eb1377
Jad Hadchity <jadhadchity4@gmail.com>
2022-03-17 09:41:09 +0200

    master

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+import 'package:ecommerce/screens/signin_screen.dart';
+import 'package:ecommerce/screens/signup_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BottomAppBar(
+        child: ElevatedButton(
+          child: Text("Logout"),
+          onPressed: () {
+            FirebaseAuth.instance.signOut().then((value) {
+              print("Signed Out");
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => SignInScreen()));
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
