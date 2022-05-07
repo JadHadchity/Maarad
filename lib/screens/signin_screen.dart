b778bd3798e4d855044c3f29c7b49d1917c72f88
Jad Hadchity <jadhadchity4@gmail.com>
2022-05-07 19:46:53 +0300

    init

--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -42,7 +42,7 @@ class _SignInScreenState extends State<SignInScreen> {
               SizedBox(
                 height: 30,
               ),
-              reusableTextField("Enter Your Email", Icons.person_outline, false,
+              reusableTextField("Enter Email", Icons.person_outline, false,
                   _emailTextController),
               SizedBox(
                 height: 20,
