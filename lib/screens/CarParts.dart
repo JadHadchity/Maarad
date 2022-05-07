f6b6b5e8a31487487aa8d1ff6a7a442bd431b3e6
Jad Hadchity <jadhadchity4@gmail.com>
2022-05-07 16:22:36 +0300

    init

--- lib/screens/CarParts.dart
+++ lib/screens/CarParts.dart
@@ -1,17 +1,10 @@
-import 'package:ecommerce/Models/Parts.dart';
+import 'package:ecommerce/Utils/constant.dart';
 import 'package:ecommerce/enums.dart';
 import 'package:ecommerce/screens/NavBar.dart';
-import 'package:ecommerce/screens/CarParts.dart';
-import 'package:ecommerce/screens/ShowRoom.dart';
-import 'package:ecommerce/screens/hondaParts.dart';
-import 'package:ecommerce/screens/signin_screen.dart';
-import 'package:ecommerce/screens/signup_screen.dart';
+
 import 'package:ecommerce/components/coustom_bottom_nav_bar.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:flutter/material.dart';
-import 'package:ecommerce/Models/parts.dart' as parts;
-import 'hondaParts.dart';
-import 'package:ecommerce/screens/hondaParts.dart';
 
 class CarParts extends StatefulWidget {
   const CarParts({Key? key}) : super(key: key);
@@ -25,41 +18,12 @@ class _CarPartsScreenState extends State<CarParts> {
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: NavBar(),
+      appBar: AppBar(
+        title: Text('Car Parts'),
+        backgroundColor: kPrimaryColor,
+      ),
       bottomNavigationBar: CustomBottomNavBar(selectedMenu: MenuState.home),
-      appBar: AppBar(title: Text('Car Parts')),
-      body: ListView.builder(
-          itemCount: partList.length,
-          itemBuilder: (context, index) {
-            Parts parts = partList[index];
-            return Card(
-                child: ListTile(
-                    title: Text(parts.title),
-                    subtitle: Text(parts.year.toString()),
-                    leading: Image.network(parts.imageUrl),
-                    trailing: Icon(Icons.arrow_forward_rounded),
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => hondaPart(parts)));
-                    }));
-          }),
+      body: Center(),
     );
   }
 }
-  
-  
-
-
-// class CarsParts extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         body: ListView.builder(
-//             itemCount: partList.length,
-//             itemBuilder: (context, index) {
-//               Parts parts = partList[index];
-//               return Text(parts.title);
-//             }));
-//   }
-// }
