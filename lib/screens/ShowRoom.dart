40dca0263af524d602050398c37aac1edd04fcde
Jad Hadchity <jadhadchity4@gmail.com>
2022-06-14 14:14:33 +0300

    init

--- lib/screens/ShowRoom.dart
+++ lib/screens/ShowRoom.dart
@@ -15,75 +15,78 @@ class ShowRoom extends StatelessWidget {
   final DataController controller = Get.put(DataController());
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: AppDrawer(),
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('All Product List'),
-        actions: [
-          IconButton(
-              onPressed: () {
-                Get.to(() => AddProductScreen());
-              },
-              icon: Icon(Icons.add))
-        ],
-      ),
-      bottomNavigationBar: CustomBottomNavBar(selectedMenu: MenuState.home),
-      body: GetBuilder<DataController>(
-        builder: (controller) => controller.allProduct.isEmpty
-            ? Center(
-                child: Text('😔 NO DATA FOUND (: 😔'),
-              )
-            : ListView.builder(
-                itemCount: controller.allProduct.length,
-                itemBuilder: (context, index) {
-                  return Card(
-                    child: Column(
-                      children: [
-                        Container(
-                          height: 200,
-                          width: double.infinity,
-                          child: Image.network(
-                            controller.allProduct[index].productimage,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Text(
-                                "Product Name: ${controller.allProduct[index].productname}",
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                              Text(
-                                'Price: ${controller.allProduct[index].productprice.toString()}',
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Expanded(
-                                child: ElevatedButton(
-                                  onPressed: () => launch(
-                                      "tel:${controller.allProduct[index].phonenumber.toString()}"),
-                                  child: Text('CALL'),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        drawer: AppDrawer(),
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('All Product List'),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Get.to(() => AddProductScreen());
                 },
-              ),
+                icon: Icon(Icons.add))
+          ],
+        ),
+        bottomNavigationBar: CustomBottomNavBar(selectedMenu: MenuState.home),
+        body: GetBuilder<DataController>(
+          builder: (controller) => controller.allProduct.isEmpty
+              ? Center(
+                  child: Text('😔 NO DATA FOUND (: 😔'),
+                )
+              : ListView.builder(
+                  itemCount: controller.allProduct.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: Column(
+                        children: [
+                          Container(
+                            height: 200,
+                            width: double.infinity,
+                            child: Image.network(
+                              controller.allProduct[index].productimage,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Text(
+                                  "Product Name: ${controller.allProduct[index].productname}",
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  'Price: ${controller.allProduct[index].productprice.toString()}',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Expanded(
+                                  child: ElevatedButton(
+                                    onPressed: () => launch(
+                                        "tel:${controller.allProduct[index].phonenumber.toString()}"),
+                                    child: Text('CALL'),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+        ),
       ),
     );
   }
